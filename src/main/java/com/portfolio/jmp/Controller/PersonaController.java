af327d8551819d39732861d6d6bb8d2349ef0ebb
Manna97 <juanmapra@yahoo.com.ar>
2022-10-31 23:41:10 -0300

    Corrigiendo creacion de persona

--- src/main/java/com/portfolio/jmp/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jmp/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,19 +51,11 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Educacion eliminada"), HttpStatus.OK);
     }*/
     
-    /*@PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody DtoEducacion dtoeducacion){
-        if(StringUtils.isBlank(dtoeducacion.getNombreEdu())){
-            return new ResponseEntity(new Mensaje("El nombre es obligatorio"),HttpStatus.BAD_REQUEST);
-        }
-        if(sEducacion.existsByNombreEdu(dtoeducacion.getNombreEdu())){
-            return new ResponseEntity(new Mensaje("Ese nombre ya existe"),HttpStatus.BAD_REQUEST);
-        }
-        
-        Educacion educacion = new Educacion(dtoeducacion.getNombreEdu(),dtoeducacion.getDescripcionEdu());
-        sEducacion.save(educacion);
-        return new ResponseEntity(new Mensaje("Educacion creada"),HttpStatus.OK);
-    }*/
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        personaService.save(persona);
+        return "La persona fue creada correctamente";
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtopersona){
